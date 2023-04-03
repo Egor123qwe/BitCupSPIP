2b8466a0b913b21b1d3b06075c0d577819da9938
Yagor Truhan <48926720+Egor123qwe@users.noreply.github.com>
2023-04-03 06:53:34 +0300

    page2nonAdaptive

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,6 @@
 const backgroundImg = document.querySelector('.background-img');
 const stages = document.querySelector('.stages');
-const footer = document.getElementsByTagName('footer')[0];
+const footer = document.querySelector('footer');
 
 function resize() {
     backgroundImg.style.height = stages.offsetHeight + footer.offsetHeight + 90 + 'px';
